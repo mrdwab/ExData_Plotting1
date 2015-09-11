06cf5e479d5babab690e4633da8844cb0f190288
Ananda Mahto <mrdwab@gmail.com>
2015-09-11 22:58:25 +0530

    corrections to plot 3 and plot 4

--- plot4.R
+++ plot4.R
@@ -13,13 +13,27 @@ library(tidyr)
 # rm(list = ls())
 
 load("data/power_consumption_feb.Rdata")
-png(filename = "plot3.png", width = 480, height = 480, units = "px")
+
+## Convert to datetime
+pcFeb <- pcFeb %>%
+  unite(DateTime, sep = " ", Date, Time) %>%
+  mutate(DateTime = as.POSIXct(strptime(DateTime, format = "%d/%m/%Y %H:%M:%S")))
 
 ## subset the data
 subMeter <- pcFeb %>%
-  unite(DateTime, sep = " ", Date, Time) %>%
-  mutate(DateTime = as.POSIXct(strptime(DateTime, format = "%d/%m/%Y %H:%M:%S"))) %>%
   select(DateTime, Sub_metering_1, Sub_metering_2, Sub_metering_3)
+
+png(filename = "plot4.png", width = 480, height = 480, units = "px")
+
+par(mfrow = c(2, 2))
+
+pcFeb %>%
+  select(DateTime, Global_active_power) %>%
+  plot(type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+pcFeb %>%
+  select(DateTime, Voltage) %>%
+  plot(type = "l", xlab = "datetime", ylab = "Voltage")
 
 ## Plot
 plot(subMeter[1:2], type = "l", xlab = "", ylab = "Energy sub metering")
@@ -27,6 +41,10 @@ points(subMeter[c(1, 3)], type = "l", col = "red")
 points(subMeter[c(1, 4)], type = "l", col = "blue")
 
 ## Add legend
-legend("topright", bty = "o", legend = names(subMeter[-1]), col = c("black", "red", "blue"), lty = 1)
+legend("topright", bty = "n", legend = names(subMeter[-1]), col = c("black", "red", "blue"), lty = 1)
+
+pcFeb %>%
+  select(DateTime, Global_reactive_power) %>%
+  plot(type = "l", xlab = "datetime", ylab = "Global_reactive_power")
 
 dev.off()
